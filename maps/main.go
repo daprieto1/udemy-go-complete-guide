564fc57ad62f9c643efdcabee41010c971097844
Diego Prieto <diegopr@microsoft.com>
2022-04-04 13:58:12 -0500

    feat: hackerrank problem

--- maps/main.go
+++ maps/main.go
@@ -1,5 +1,98 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func cookies(k int, A []int) int {
+	var op int = 0
+
+	h := IntHeap(A)
+	heap.Init(&h)
+
+	for len(h) > 1 && h[0] < k {
+		op++
+		nc := heap.Pop(&h).(int) + (2 * heap.Pop(&h).(int))
+		heap.Push(&h, nc)
+		//fmt.Println(h)
+	}
+
+	return op
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create("C:\\Users\\diegopr\\Code\\Udemy\\udemy-go-complete-guide\\maps\\output")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int(kTemp)
+
+	ATemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var A []int
+
+	for i := 0; i < int(n); i++ {
+		AItemTemp, err := strconv.ParseInt(ATemp[i], 10, 64)
+		checkError(err)
+		AItem := int(AItemTemp)
+		A = append(A, AItem)
+	}
+
+	result := cookies(k, A)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
